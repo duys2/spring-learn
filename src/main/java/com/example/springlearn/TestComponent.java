a74dc5d5198d7058c35caecec64b4b276f48463f
Ays <minigun5@naver.com>
2024-07-12 15:21:55 +0900

    [Comment] 설명 주석 추가

--- src/main/java/com/example/springlearn/TestComponent.java
+++ src/main/java/com/example/springlearn/TestComponent.java
@@ -3,11 +3,14 @@ package com.example.springlearn;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+// Spring 애플리케이션의 설정 값을 주입받는 예시
 @Component
 public class TestComponent {
+	// 서버의 포트 번호 주입받기: application.properties 또는 application.yml 파일의 server.port 값 사용
 	@Value("${server.port}")
 	private int port;
 
+	// 스프링 애플리케이션의 이름 주입받기: application.properties 또는 application.yml 파일의 spring.application.name 값 사용
 	@Value("${spring.application.name}")
 	private String appName;
 }
