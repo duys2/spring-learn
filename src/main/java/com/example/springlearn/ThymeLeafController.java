7fc8caae39d59adebc6de0f3bb078fca39486828
Ays <minigun5@naver.com>
2024-07-12 18:21:34 +0900

    [Add] SpringBoot + Thymeleaf 내용 추가 및 수정
    추가:
    @GetMapping("/index.html")
    public String index(Model model) {}

--- src/main/java/com/example/springlearn/ThymeLeafController.java
+++ src/main/java/com/example/springlearn/ThymeLeafController.java
@@ -1,5 +1,6 @@
 package com.example.springlearn;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeLeafController {
+	// application.yml에서 설정된 애플리케이션 이름 주입
+	@Value("${spring.application.name}")
+	private String appName;
+
+	@GetMapping("/index.html")
+	public String index(Model model) {
+		model.addAttribute("text", "index.html");
+		model.addAttribute("name", appName);
+
+		return "index";
+	}
+
 	@GetMapping("/mapping1")
-	public String example(Model model) {
+	public String mapping1(Model model) {
 		// 변수는 이름, 값의 쌍으로 추가
 		model.addAttribute("text", "백엔드 오르미");
 		model.addAttribute("name", "홍길동");
@@ -19,7 +32,7 @@ public class ThymeLeafController {
 	@GetMapping("/mapping2")
 	// ?name=값 ← 여기서 값이 아래 파라미터 name에 들어감
 	// @RequestParam에서 name2로 바꾸면 ?name2=값 ← 이렇게 해야 함
-	public String index(@RequestParam("name") String name, Model model) {
+	public String mapping(@RequestParam("name") String name, Model model) {
 		model.addAttribute("text", "동적 페이지");
 		model.addAttribute("name", name);
 		return "index";
